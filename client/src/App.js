d2a70c971f5b61c81095adbfe62dcb22571daaf7
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-03-13 17:04:28 +0100

    Landing Page - Styled Components created

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import Landing from './pages/Landing';
 function App() {
   return (
     <div>
-      <h1>Ούλε τε και μάλα χαίρε</h1>
       <Landing />
     </div>
   );
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,9 +1,10 @@
 import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
+import styled from 'styled-components';
 
 const Landing = () => {
   return (
-    <main>
+    <Wrapper>
       <nav>
         <img src={logo} alt="ErgasiaMania" className="logo" />
       </nav>
@@ -21,10 +22,48 @@ const Landing = () => {
           </p>
           <button className="btn btn-hero">Login/Register</button>
         </div>
+        <img src={main} alt="Job hunt" className="img main-img" />
       </div>
-      <img src={main} alt="Job hunt" className="img main-img" />
-    </main>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
 
 export default Landing;
