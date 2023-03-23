c70414a6127d2d18fd5eae561b7335727930dec4
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-03-23 19:30:57 +0100

    Small Sidebar & Big Sidebar

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,9 +1,24 @@
 import styled from 'styled-components';
+import { useAppContext } from '../context/appContext';
+import NavLinks from './NavLinks';
+import Logo from '../components/Logo';
 
 const BigSidebar = () => {
+  const { showSidebar } = useAppContext();
   return (
     <Wrapper>
-      <div>Big Sidebar</div>
+      <div
+        className={
+          showSidebar ? 'sidebar-container ' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
