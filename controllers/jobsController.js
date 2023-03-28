34f0efdf0fbaf173ff702059914db8631adfee26
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-03-28 19:28:23 +0200

    Get All jobs - Back End

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,17 +1,17 @@
-import Job from '../models/Job.js'
+import Job from '../models/Job.js';
 import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, NotFoundError } from '../errors/index.js';
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
-
   if (!position || !company) {
     throw new BadRequestError('Συμπλήρωσε όλα τα πεδία! (JobController)');
   }
-
+  
   req.body.createdBy = req.user.userId;
 
   const job = await Job.create(req.body);
+  
   res.status(StatusCodes.CREATED).json({ job });
 };
 
@@ -22,7 +22,10 @@ const updateJob = async (req, res) => {
   res.send('Update Job');
 };
 const getAllJobs = async (req, res) => {
-  res.send('Get All Jobs');
+  const jobs = await Job.find({ createdBy: req.user.userId });
+  res
+    .status(StatusCodes.OK)
+    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
 };
 const showStats = async (req, res) => {
   res.send('Show Stats');
