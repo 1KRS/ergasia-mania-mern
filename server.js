5601566b692973eafda243b15a7759b7846c9f8f
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-04-03 21:36:06 +0200

    Build FE Application

--- server.js
+++ server.js
@@ -9,11 +9,16 @@ import authRouter from './routes/authRoutes.js';
 import morgan from 'morgan';
 import authenticateUser from './middleware/auth.js';
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Authentication | Αυθεντικοποίηση Χρήστη
 
@@ -24,14 +29,23 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'))
 }
 
+// Only when ready to deploy
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
 app.use(express.json()); // Κάνει διαθέσιμα τα στοιχεία JSON στις διαδρομές μας παρακάτω (λόγω POST request)
 
 app.get('/api/v1', (req, res) => {
   res.send('Ούλε τε και μάλα χαίρε!');
 });
 
+// Routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+
+// Only when ready to deploy
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
